8d79d1b85649793806e497c9aa420c714163bb8c
Julia <julia.angelina.grossi@gmail.com>
2025-01-13 19:24:06 +0100

    change the README

--- Back/obj/Debug/net8.0/back.AssemblyInfo.cs
+++ Back/obj/Debug/net8.0/back.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05933056587c45374593a3b24b6aec3d725667e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f433da086d816b3041d4f0edac476c70d748f10d")]
 [assembly: System.Reflection.AssemblyProductAttribute("back")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-Pour lancer le projet : 
+Pour lancer le projet :
 
-1/ Lancer le back à la racine du projet : `dotnet run --project Back\back.csproj`
+1/ Lancer le back à la racine du projet : `dotnet run --project back.csproj`
 
 2/ Se mettre sur le dossier front : `cd Front`
 
